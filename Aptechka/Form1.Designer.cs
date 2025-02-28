151bde67f1a697c5c9fcb750587f2668a1eca3c1
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2025-02-28 14:02:31 +0500

    Add files via upload

---
+++ Aptechka/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Aptechka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(642, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(548, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Поиск";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(775, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17});
+            this.dataGridView1.Location = new System.Drawing.Point(38, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(2404, 693);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Антибактериальные препараты";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Гормоны";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Диагностические средства";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Препараты, влияющие на иммунитет";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Препараты влияющие на метаболизм";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Препараты влияющие на психику";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Препараты, влияющие на свертываемость крови";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Препараты, влияющие на тонус сосудов";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Препараты, влияющие на функцию бронхов";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Препараты, влияющие на функции желудочно-кишечного тракта";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 125;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Препараты, влияющие на функции миокарда";
+            this.Column11.MinimumWidth = 6;
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 125;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = " Препараты, влияющие на функцию почек";
+            this.Column12.MinimumWidth = 6;
+            this.Column12.Name = "Column12";
+            this.Column12.Width = 125;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Противовирусные препараты";
+            this.Column13.MinimumWidth = 6;
+            this.Column13.Name = "Column13";
+            this.Column13.Width = 125;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Противовоспалительные и обезболивающие препараты";
+            this.Column14.MinimumWidth = 6;
+            this.Column14.Name = "Column14";
+            this.Column14.Width = 125;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Противогрибковые препараты";
+            this.Column15.MinimumWidth = 6;
+            this.Column15.Name = "Column15";
+            this.Column15.Width = 125;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Противоопухолевые препараты";
+            this.Column16.MinimumWidth = 6;
+            this.Column16.Name = "Column16";
+            this.Column16.Width = 125;
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Противопаразитарные и противоглистные препараты";
+            this.Column17.MinimumWidth = 6;
+            this.Column17.Name = "Column17";
+            this.Column17.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2510, 888);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+    }
+}
+
---
+++ Aptechka/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Aptechka
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectToDatabase();
+            LoadMedicinesToDataGridView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ConnectToDatabase()
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDb;Initial Catalog=Pharmacy;Integrated Security=True"; // Укажите ваш сервер
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+         }
+        private void LoadMedicinesToDataGridView()
+        {
+            var medicines = LoadMedicinesFromDatabase();
+
+            // Очищаем DataGridView перед добавлением новых данных
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Добавляем заголовки для категорий
+            string[] categories = new string[]
+            {
+                "Антибактериальные препараты",
+                "Гормоны",
+                "Диагностические средства",
+                "Препараты, влияющие на иммунитет",
+                "Препараты, влияющие на метаболизм",
+                "Препараты, влияющие на психику",
+                "Препараты, влияющие на свертываемость крови",
+                "Препараты, влияющие на тонус сосудов",
+                "Препараты, влияющие на функцию бронхов",
+                "Препараты, влияющие на функции желудочно-кишечного тракта",
+                "Препараты, влияющие на функции миокарда",
+                "Препараты, влияющие на функцию почек",
+                "Противовирусные препараты",
+                "Противовоспалительные и обезболивающие препараты",
+                "Противогрибковые препараты",
+                "Противоопухолевые препараты",
+                "Противопаразитарные и противоглистные препараты"
+            };
+
+            // Добавляем 17 колонок с названиями категорий
+            for (int i = 0; i < categories.Length; i++)
+            {
+                dataGridView1.Columns.Add("Category" + (i + 1), categories[i]);
+            }
+
+                // Устанавливаем ширину заголовка строк
+                dataGridView1.RowHeadersWidth = 80;
+
+                // Устанавливаем высоту строк
+                dataGridView1.RowTemplate.Height = 50; // Установите желаемую высоту
+            
+                // Группируем лекарства по полкам
+                var groupedMedicines = medicines.GroupBy(m => m.Location);
+
+            // Добавляем строки для каждой полки
+            foreach (var group in groupedMedicines)
+            {
+                // Добавляем новую строку для текущей полки
+                int rowIndex = dataGridView1.Rows.Add(); // Добавляем новую строку
+                dataGridView1.Rows[rowIndex].HeaderCell.Value = group.Key; // Устанавливаем название полки
+
+                // Заполняем ячейки названиями препаратов по категориям для текущей полки
+                foreach (var medicine in group)
+                {
+                    // Находим индекс категории
+                    int categoryIndex = Array.IndexOf(categories, medicine.Category);
+                    if (categoryIndex >= 0)
+                    {
+                        // Устанавливаем название препарата в соответствующую ячейку
+                        dataGridView1.Rows[rowIndex].Cells[categoryIndex].Value = medicine.Name;
+                    }
+                }
+            }
+
+            // Сортировка по названию полки
+            //dataGridView1.Sort(dataGridView1.Columns[0], System.ComponentModel.ListSortDirection.Ascending);
+        }
+
+
+        private List<Medicine> LoadMedicinesFromDatabase()
+        {
+            List<Medicine> medicines = new List<Medicine>();
+
+            string query = "SELECT Name, Category, Form, Location FROM Pharmacy"; // Убедитесь, что здесь правильное имя таблицы
+            SqlCommand command = new SqlCommand(query, connection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows) // Проверяем, есть ли строки
+                {
+                    while (reader.Read())
+                    {
+                        medicines.Add(new Medicine
+                        {
+                            Name = reader["Name"].ToString(),
+                            Category = reader["Category"].ToString(),
+                            Form = reader["Form"].ToString(),
+                            Location = reader["Location"].ToString() // Сохраняем название полки
+                        });
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Нет данных в таблице Pharmacy."); // Уведомление, если данных нет
+                }
+            }
+
+            return medicines;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string searchTerm = textBox1.Text.ToLower();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[0].Value.ToString().ToLower().Contains(searchTerm))
+                {
+                    row.Selected = true; // Подсвечиваем найденную строку
+                    row.DefaultCellStyle.BackColor = Color.Yellow; // Меняем цвет фона
+                }
+                else
+                {
+                    row.Selected = false; // Убираем выделение, если не найдено
+                    row.DefaultCellStyle.BackColor = Color.White; // Возвращаем цвет фона
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public class Medicine
+        {
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public string Form { get; set; }
+            public string Location { get; set; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text.ToLower();
+
+            // Сбрасываем цвет ячеек перед новым поиском
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    cell.Style.BackColor = Color.White; // Устанавливаем белый цвет для всех ячеек
+                }
+            }
+
+            // Если текст для поиска не пустой
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                // Проходим по всем строкам и ячейкам
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        // Проверяем, содержит ли ячейка текст для поиска
+                        if (cell.Value != null && cell.Value.ToString().ToLower().Contains(searchText))
+                        {
+                            // Выделяем ячейку цветом
+                            cell.Style.BackColor = Color.Yellow; // Вы можете выбрать любой другой цвет
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
